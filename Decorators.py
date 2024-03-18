b54ae92dd8bc87f3847c0c9b2e10d5f2d6788c32
Ankit <129873373+fnuankit164@users.noreply.github.com>
2024-03-17 21:31:34 -0400

    Update Decorators.py

--- Decorators.py
+++ Decorators.py
@@ -1,3 +1,12 @@
+class decorator_class(object):
+
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print('call method before {}'.format(self.original_function.__name__))
+        self.original_function(*args, **kwargs)
+
 # Decorators
 from functools import wraps
 
